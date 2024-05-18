17f9c3a9429279775cd9b9976dd398a8d8978acd
Passeriade <manuel5mongelos@gmail.com>
2024-05-18 18:55:21 -0300

    Added Db context

---
+++ PencaUcuApi/Data/MyDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using PencaUcuApi.Models;
+public class MyDbContext : DbContext
+{
+    public DbSet<Administrator> Administrators { get; set; }
+    public DbSet<Career> Careers { get; set; }
+    public DbSet<Match> Matches { get; set; }
+    public DbSet<MatchResult> MatchResults { get; set; }
+    public DbSet<NationalTeam> NationalTeams { get; set; }
+    public DbSet<NationalTeamGroupStage> NationalTeamGroupStages { get; set; }
+    public DbSet<Prediction> Predictions { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<StudentCareer> StudentCareers { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+}
