2be5fc6fa3d35aaa1054c6633ceb6f236188946a
Emiliano Labarthe <emilianolabarthe123@gmail.com>
2024-05-24 19:34:03 -0300

    fix in login fields

--- penca-angular-app/src/app/components/login/login.component.ts
+++ penca-angular-app/src/app/components/login/login.component.ts
@@ -13,42 +13,57 @@ import { IUser } from '../../interfaces/IUser';
 })
 export class LoginComponent {
 
-  model = { Id: 'Cédula de Identidad', Password: 'Contraseña', Type: 'Tipo de usuario' };
+  model = { Id: '', Password: '', Type: '' };
   userTypes= ['Administrador', 'Estudiante'];
+  
+  falseType = false;
+  falseId = false;
+  falsePass = false;
 
   constructor(private studentService: StudentService, private adminService: AdministratorService) { }
 
   login(): void {
-    if(this.model.Type.localeCompare(this.userTypes[0])) { // Administrator login
-      this.adminService.login(this.model.Id, this.model.Password)
-      .pipe(
-        catchError((error) => {
-          console.error(error);
-          alert('Error al iniciar sesión como administrador. Por favor, vuelva a intentar.');
-          throw error;
-        })
-      ).subscribe({
-        next: (response: IUser) => {
-          alert('Inicio de sesión aprobado. ¡Bienvenido!');
-          this.model = { Id: 'Cédula de Identidad', Password: 'Contraseña', Type: 'Tipo de usuario' };
-        }
-      });
-    } else { // Student login
-      this.studentService.login(this.model.Id, this.model.Password)
-      .pipe(
-        catchError((error) => {
-          console.error(error);
-          alert('Error al iniciar sesión como estudiante. Por favor, vuelva a intentar.');
-          throw error;
-        })
-      ).subscribe({
-        next: (response: IStudent) => {
-          alert('Inicio de sesión aprobado. ¡Bienvenido!');
-          this.model = { Id: 'Cédula de Identidad', Password: 'Contraseña', Type: 'Tipo de usuario' };
-        }
-      });
+    if(this.model.Id && this.model.Password && this.model.Type){
+      if(this.model.Type.localeCompare(this.userTypes[0])) { // Administrator login
+        this.adminService.login(this.model.Id, this.model.Password)
+        .pipe(
+          catchError((error) => {
+            console.error(error);
+            alert('Error al iniciar sesión como administrador. Por favor, vuelva a intentar.');
+            throw error;
+          })
+        ).subscribe({
+          next: (response: IUser) => {
+            alert('Inicio de sesión aprobado. ¡Bienvenido!');
+            this.model = { Id: '', Password: '', Type: '' };
+          }
+        });
+      } else { // Student login
+        this.studentService.login(this.model.Id, this.model.Password)
+        .pipe(
+          catchError((error) => {
+            console.error(error);
+            alert('Error al iniciar sesión como estudiante. Por favor, vuelva a intentar.');
+            throw error;
+          })
+        ).subscribe({
+          next: (response: IStudent) => {
+            alert('Inicio de sesión aprobado. ¡Bienvenido!');
+            this.model = { Id: 'Cédula de Identidad', Password: 'Contraseña', Type: 'Tipo de usuario' };
+          }
+          
+        });
+      }
+    }if(!this.model.Id){
+      this.falseId = true;
+    }if(!this.model.Type){
+      this.falseType = true;
+    }if(!this.model.Password){
+      this.falsePass = true;
     }
   }
+
+  
 
   submitted = false;
   onSubmit() { this.submitted = true; }
