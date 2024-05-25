329b21d2a470e300b82d27981ef1dfa504eead03
Passeriade <manuel5mongelos@gmail.com>
2024-05-25 02:01:16 -0300

    Controllers, boilerplate code, added to context

---
+++ PencaUcuApi/Controllers/AdminController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PencaUcuApi.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class AdminController : ControllerBase
+{
+    private readonly MyDbContext _dbContext;
+    private readonly ILogger<UserController> _logger;
+
+    public AdminController(ILogger<UserController> logger, MyDbContext dbContext)
+    {
+        _logger = logger;
+        _dbContext = dbContext;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        // TODO: Implement your logic here
+        return Ok("Get method called");
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody] object data)
+    {
+        // TODO: Implement your logic here
+        return Ok("Post method called");
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, [FromBody] object data)
+    {
+        // TODO: Implement your logic here
+        return Ok($"Put method called with id: {id}");
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        // TODO: Implement your logic here
+        return Ok($"Delete method called with id: {id}");
+    }
+}
--- PencaUcuApi/Data/MyDbContext.cs
+++ PencaUcuApi/Data/MyDbContext.cs
@@ -5,14 +5,17 @@ public class MyDbContext : DbContext
     public DbSet<User> Users { get; set; }
     public DbSet<Administrator> Administrators { get; set; }
     public DbSet<Career> Careers { get; set; }
+    public DbSet<GroupStage> GroupStages { get; set; }
     public DbSet<Match> Matches { get; set; }
     public DbSet<MatchResult> MatchResults { get; set; }
     public DbSet<NationalTeam> NationalTeams { get; set; }
     public DbSet<NationalTeamGroupStage> NationalTeamGroupStages { get; set; }
     public DbSet<Prediction> Predictions { get; set; }
+    public DbSet<Stadium> Stadiums { get; set; }
+    public DbSet<Stage> Stages { get; set; }
     public DbSet<Student> Students { get; set; }
     public DbSet<StudentCareer> StudentCareers { get; set; }
-
+    public DbSet<StudentTournamentPrediction> StudentTournamentPredictions { get; set; }
     public MyDbContext(DbContextOptions<MyDbContext> options)
         : base(options)
     {
