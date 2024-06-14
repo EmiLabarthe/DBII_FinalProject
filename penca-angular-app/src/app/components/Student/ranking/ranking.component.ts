f631601247a2ef04832e007d27ea8ae717a96781
Emiliano Labarthe <emilianolabarthe123@gmail.com>
2024-06-14 16:53:30 -0300

    ranking working, still needs design

--- penca-angular-app/src/app/components/Student/ranking/ranking.component.ts
+++ penca-angular-app/src/app/components/Student/ranking/ranking.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IRankingItem } from 'src/app/interfaces/IRankingItem';
+import { RankingService } from 'src/app/services/ranking.service';
 
 @Component({
   selector: 'app-ranking',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./ranking.component.css']
 })
 export class RankingComponent {
+
+  ranking:  IRankingItem[] | undefined;
+
+  constructor(private route: ActivatedRoute, private rankingService: RankingService){}
+
+  ngOnInit(): void{
+    this.rankingService.getRanking().subscribe(
+      (data: IRankingItem[]) => {
+        this.ranking = data;
+      },
+      (error) => {
+        console.error('Error fetching ranking', error)
+      }
+    )
+  }
 
 }
