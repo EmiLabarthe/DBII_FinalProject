acf4efe4d007892738c8a9fee0054ec593953bc8
Emiliano Labarthe <emilianolabarthe123@gmail.com>
2024-06-22 14:45:50 -0300

    front de admin

--- penca-angular-app/src/app/components/Admin/upload-result/upload-result.component.ts
+++ penca-angular-app/src/app/components/Admin/upload-result/upload-result.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { IMatch } from 'src/app/interfaces/IMatch';
 import { MatchService } from 'src/app/services/match.service';
 import { CommonModule } from '@angular/common';
+import { IMatchResult } from 'src/app/interfaces/IMatchResult';
+import { MatchResultService } from 'src/app/services/match-result.service';
 
 @Component({
   selector: 'app-upload-result',
@@ -11,10 +13,28 @@ import { CommonModule } from '@angular/common';
 export class UploadResultComponent {
 
   matches: any[] | undefined;
+  teams: string[] = [
+    "Argentina",
+    "Canadá",
+    "Chile",
+    "Perú",
+    "Ecuador",
+    "Jamaica",
+    "México",
+    "Venezuela",
+    "Bolivia",
+    "Estados Unidos",
+    "Panamá",
+    "Uruguay",
+    "Brasil",
+    "Colombia",
+    "Costa Rica",
+    "Paraguay"
+  ];
 
-  constructor(private matchService: MatchService){}
+  model = { MatchId: '', LocalNationalTeamGoals: 0, VisitorNationalTeamGoals: 0, WinnerId: '' };
 
-
+  constructor(private matchService: MatchService, private matchResultService: MatchResultService){}
 
   ngOnInit(){
     this.matchService.getMatches().subscribe(
@@ -26,4 +46,17 @@ export class UploadResultComponent {
       }
     )
   }
+
+  submit(){
+    if(this.model.MatchId !== null && this.model.LocalNationalTeamGoals !== null && this.model.VisitorNationalTeamGoals !== null && this.model.WinnerId !== null){
+      this.matchResultService.addResult(this.model.MatchId, this.model.LocalNationalTeamGoals, this.model.VisitorNationalTeamGoals, this.model.WinnerId)
+      .subscribe(res =>{
+        console.log(`resultado agregado correctamente, ${res}`);
+        alert('Resultado agregado!');
+        this.model = { MatchId: '', LocalNationalTeamGoals: 0, VisitorNationalTeamGoals: 0, WinnerId: '' };
+      })
+    }
+  }
+
+  onSubmit(){}
 }
